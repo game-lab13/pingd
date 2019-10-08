81c3005dcc6d3000aaad2b1eeda597ca171ea094
Duane McFarlane <duanemcfarlane@yahoo.com>
2019-10-07 20:43:33 -0400

    set up App.jsx component

---
+++ app/Components/App.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+    // state = {  }
+    render() { 
+        return ( 
+            <div>
+                <p>From App Component</p>
+            </div>
+         );
+    }
+}
+ 
+export default App;
+
+ 
---
+++ app/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './Components/App.jsx'
+import './index.scss'
+
+ReactDOM.render(<App />, document.getElementById('app'))
