15ad2eaca17c382b26265ae02d23359fa1d8d81e
xak200 <xak200@nyu.edu>
2019-10-09 16:16:40 -0400

    Added functionality to send pending invites and scores
    Co-authored-by: nmczormick <umanzor2949@gmail.com>
    Co-authored-by: xak200 <xariak91@gmail.com>

--- server/controllers/matchController.js
+++ server/controllers/matchController.js
@@ -1,10 +1,66 @@
+const pool = require('../db');
+
 const matchController = {};
 
-matchController.getPendingInvites = () => {
+matchController.fetchInvitesReceived = (req, res, next) => {
+    const guest_id = res.locals.userInfo['id'];
+    const inviteQuery = `SELECT first_name, username, "user".id FROM "user" INNER JOIN "match" ON "user".id="match".host_id WHERE invite_status='pending' AND guest_id=$1;`;
+    const userIdValue = [guest_id];
+
+    pool.query(inviteQuery, userIdValue, (err, result) => {
+        if (err) {
+            res.status(500).json(err);
+        } else {
+            res.locals['invitesReceived'] = result.rows;
+            next()
+        }
+    });
+}
+
+matchController.fetchInvitesSent = (req, res, next) => {
+    const host_id = res.locals.userInfo['id'];
+    const inviteQuery = `SELECT first_name, username, "user".id FROM "user" INNER JOIN "match" ON "user".id="match".guest_id WHERE invite_status='pending' AND host_id=$1;`;
+    const userIdValue = [host_id];
+
+    pool.query(inviteQuery, userIdValue, (err, result) => {
+        if (err) {
+            res.status(500).json(err);
+        } else {
+            res.locals['invitesSent'] = result.rows;
+            next();
+        }
+    });
+}
+
+matchController.fetchScoresToRecord = (req, res, next) => {
+    const host_id = res.locals.userInfo['id'];
+    const inviteQuery = `SELECT first_name, username, "user".id FROM "user" INNER JOIN "match" ON "user".id="match".guest_id WHERE score_status='pending' AND host_id=$1;`;
+    const userIdValue = [host_id];
+
+    pool.query(inviteQuery, userIdValue, (err, result) => {
+        if (err) {
+            res.status(500).json(err);
+        } else {
+            res.locals['scoresToRecord'] = result.rows;
+            next()
+        }
+    });
 
 }
 
-matchController.getPendingScoreConfirmations = () => {
+matchController.fetchScoresToConfirm = (req, res, next) => {
+    const guest_id = res.locals.userInfo['id'];
+    const inviteQuery = `SELECT first_name, username, "user".id, selected_winner FROM "user" INNER JOIN "match" ON "user".id="match".host_id WHERE score_status='hostConfirmed' AND guest_id=$1;`;
+    const userIdValue = [guest_id];
+
+    pool.query(inviteQuery, userIdValue, (err, result) => {
+        if (err) {
+            res.status(500).json(err);
+        } else {
+            res.locals['scoresToConfirm'] = result.rows;
+            res.status(200).json(res.locals)
+        }
+    });
 
 }
 
